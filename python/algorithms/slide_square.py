d27b98fdd65fcf13749d93a8c27fb5c1c4efb22a
william cardoso <williancard123@gmail.com>
2025-01-24 13:41:49 -0400

    refactor

--- python/algorithms/slide_square.py
+++ python/algorithms/slide_square.py
@@ -2,16 +2,23 @@ from .base_algorithm import BaseAlgorithm
 from ultralytics import solutions
 import cv2
 import numpy as np
+from skimage import io, exposure
 
 
 class DeepAlgorithmV2(BaseAlgorithm):
     def __init__(self, square_size=256):
         self.square_size = square_size
 
+    def process_image(self, image):
+        increased_contrast = exposure.adjust_gamma(image, gamma=1.5)
+
+        return increased_contrast
+
     def process_square(self, square):
-       
+
         model_path = './algorithms/models_ai/scanner-model.pt'
-        region = [(0, 0), (self.square_size, 0), (self.square_size, self.square_size), (0, self.square_size)]
+        region = [(0, 0), (self.square_size, 0), (self.square_size,
+                                                  self.square_size), (0, self.square_size)]
         counter = solutions.ObjectCounter(
             model=model_path, region=region, show=False)
 
@@ -19,5 +26,5 @@ class DeepAlgorithmV2(BaseAlgorithm):
         boxes_list = []
         if len(counter.boxes) > 0:
             boxes_list = counter.boxes.tolist()
-    
+
         return len(counter.boxes), counted_image, boxes_list
